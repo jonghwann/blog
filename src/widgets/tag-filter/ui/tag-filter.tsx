8e4409ff75edffec1a9f251ed2d9c6fdcc0e6a94
jonghwann <jonghwan0729@gmail.com>
2025-10-13 17:00:34 +0900

    refactor: improve tag-filter logic

--- src/widgets/tag-filter/ui/tag-filter.tsx
+++ src/widgets/tag-filter/ui/tag-filter.tsx
@@ -1,6 +1,6 @@
 import type { Tag } from '@/entities/tag';
 import { cn } from '@/shared/lib';
-import { Tag as TagComponent } from '@/shared/ui';
+import { TagLink } from '@/shared/ui';
 
 interface TagFilterProps {
   tags: Tag[];
@@ -11,13 +11,18 @@ interface TagFilterProps {
 export function TagFilter({ tags, tag, className }: TagFilterProps) {
   return (
     <ul className={cn('flex flex-wrap items-center gap-2', className)}>
-      {tags.map(({ name, count }) => (
-        <li key={name}>
-          <TagComponent href={tag === name ? '/tags' : `/tags/${name}`} isActive={tag === name}>
-            {name} {count && `(${count})`}
-          </TagComponent>
-        </li>
-      ))}
+      {tags.map(({ name, count }) => {
+        const isActive = tag === name;
+        const href = isActive ? '/tags' : `/tags/${name}`;
+
+        return (
+          <li key={name}>
+            <TagLink href={href} isActive={isActive}>
+              {name} {count && `(${count})`}
+            </TagLink>
+          </li>
+        );
+      })}
     </ul>
   );
 }
